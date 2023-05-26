4f917d99a87bf31d77789e070b4e8aecd1dc26bd
ShivangM <shivangmishra0824@gmail.com>
2023-05-26 21:00:11 +0530

    Gradient BG

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        gradient: {
+          '0%': { transform: 'bg-top' },
+          '50%': { transform: 'bg-left' },
+          '100%': { transform: 'bg-top' },
+        },
+      },
+      animation: {
+        gradient: 'gradient 6s ease infinite',
+      },
     },
   },
   plugins: [],
-}
+};
